899c8baa67c36aadcd692ebfc74544b039ecf9ca
WhyNotRunning <799038015@qq.com>
2016-01-30 16:12:40 +0800

    first commit

---
+++ mavenjava/src/main/java/com/iwooto/util/Java.java
@@ -0,0 +1,10 @@
+package com.iwooto.util;
+
+public class Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("java");
+	}
+
+}
---
+++ mavenweb/src/main/java/com/iwooto/web/Web.java
@@ -0,0 +1,10 @@
+package com.iwooto.web;
+
+public class Web {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("web");
+	}
+
+}
